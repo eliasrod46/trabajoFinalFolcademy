faaeaa48207c770f768dd043be4edf0a216bcb71
Elias Rodriguez <eliasrod46@gmail.com>
2022-06-30 21:09:24 -0300

    Desafio 1

--- suma.ts
+++ suma.ts
@@ -1,5 +1,20 @@
-function suma (a:number, b:number){
+function suma(a: number, b:number){
     return a+b;
 }
+function resta(a: number, b:number){
+    return a-b;
+}
+function multip(a: number, b:number){
+    return a*b;
+}
+function div(a: number, b:number){
+    return a/b;
+}
 
-console.log(suma(4,4))
+let num1 = 2
+let num2 = 8
+
+console.log(`La suma de ${num1} + ${num2} es: ${suma(num1,num2)}`);
+console.log(`La resta de ${num1} - ${num2} es: ${resta(num1,num2)}`);
+console.log(`La multiplicacion de ${num1} x ${num2} es: ${multip(num1,num2)}`);
+console.log(`La division de ${num1} / ${num2} es: ${div(num1,num2)}`);
