a6668b5a8fab096cad2fd5dff9540dd64024f1c7
Elias Rodriguez <eliasrod46@gmail.com>
2022-07-04 22:01:08 -0300

    desafio2-partial

---
+++ server.ts
@@ -0,0 +1,55 @@
+//--------------------------------REQUIRES
+import express from 'express'
+import * as productos from './productos'
+
+//Instanciamos el server dde express 
+const app = express()
+
+
+//middlewears
+app.use(express.json());
+
+
+//--------------------------------Rutas
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/productos', (req, res)=> {
+  //Lista todos los productos
+  res.json({"respuesta" : productos.getStock()}) 
+})
+
+app.post('/productos', (req, res)=> {
+  //Agrega un producto
+  //guardo lo recibido por body
+  const newProd = req.body
+  let respuesta = productos.addProduct(newProd)
+  res.json({"respuesta" : respuesta}) 
+   
+})
+
+app.put('/productos',(req, res)=> {
+  //Actualiza un producto
+  //edito el producto recibido
+  const newProd = req.body
+  let respuesta = productos.editProduct(newProd)
+  res.json({"respuesta" : respuesta})  
+})
+
+app.delete('/productos/:name',(req, res)=> {
+  //Elimina un producto
+  //guardo lo recibido por params
+  const {name} = req.params
+  let respuesta = productos.delProduct(name)
+  res.json({"respuesta" : respuesta}) 
+})
+
+
+//--------------------------------Levanto el Server
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`)
+})
